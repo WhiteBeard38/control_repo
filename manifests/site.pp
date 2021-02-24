ccd96e7b57bc8f17f7e4c3096432d5e143af9d29
WhiteBeard38 <79584800+WhiteBeard38@users.noreply.github.com>
2021-02-24 17:32:05 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,2 @@
 node default {
-  file {'/root/README':
-    ensure  => file,
-    content => 'This is README file',
-    owner   => 'root',
-  }
-  file { '/root/README':
-    owner => 'root',
-  }
 }
