cff11a9e00b9313bada17cc42604011bf60fce5a
WhiteBeard38 <79584800+WhiteBeard38@users.noreply.github.com>
2021-02-27 21:25:29 +0100

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -17,7 +17,7 @@ class minecraft {
     ensure => file,
     source => 'puppet:///modules/minecraft/minecraft.service',
   }
-  sercice { 'minecraft':
+  service { 'minecraft':
     ensure => running,
     enable => true,
   }  
